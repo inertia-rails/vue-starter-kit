8b0b9301cc2efcdc300481a79bb32816552f24f0
Justin Valentini <justin.valentini@oracle.com>
2025-12-10 17:26:34 -0500

    inertia-rails vue defaults

--- app/frontend/entrypoints/inertia.ts
+++ app/frontend/entrypoints/inertia.ts
@@ -1,17 +1,11 @@
-import { createInertiaApp } from "@inertiajs/vue3"
-import type { DefineComponent } from "vue"
-import { createApp, h } from "vue"
-
-import { initializeTheme } from "@/composables/useAppearance"
-import PersistentLayout from "@/layouts/PersistentLayout.vue"
-
-const appName = import.meta.env.VITE_APP_NAME ?? "Vue Starter Kit"
+import { createInertiaApp } from '@inertiajs/vue3'
+import { createApp, DefineComponent, h } from 'vue'
 
 createInertiaApp({
   // Set default page title
   // see https://inertia-rails.dev/guide/title-and-meta
   //
-  title: (title) => (title ? `${title} - ${appName}` : appName),
+  // title: title => title ? `${title} - App` : 'App',
 
   // Disable progress bar
   //
@@ -19,39 +13,23 @@ createInertiaApp({
   // progress: false,
 
   resolve: (name) => {
-    const pages = import.meta.glob<DefineComponent>("../pages/**/*.vue", {
+    const pages = import.meta.glob<DefineComponent>('../pages/**/*.vue', {
       eager: true,
     })
-    const page = pages[`../pages/${name}.vue`]
-    if (!page) {
-      console.error(`Missing Inertia page component: '${name}.vue'`)
-    }
+    return pages[`../pages/${name}.vue`]
 
     // To use a default layout, import the Layout component
-    // and use the following line.
+    // and use the following lines.
     // see https://inertia-rails.dev/guide/pages#default-layouts
     //
-    page.default.layout ??= PersistentLayout
-
-    return page
+    // const page = pages[`../pages/${name}.vue`]
+    // page.default.layout = page.default.layout || Layout
+    // return page
   },
 
   setup({ el, App, props, plugin }) {
-    // Uncomment the following to enable SSR hydration:
-    // if (el.hasChildNodes()) {
-    //   createSSRApp({ render: () => h(App, props) })
-    //     .use(plugin)
-    //     .mount(el)
-    //   return
-    // }
     createApp({ render: () => h(App, props) })
       .use(plugin)
       .mount(el)
   },
-  progress: {
-    color: "#4B5563",
-  },
 })
-
-// This will set light / dark mode on page load...
-initializeTheme()
